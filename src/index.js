19c0a23011d94632855545e87b810f0d233a0aea
Donovan Hiland <donovan.hiland@gmail.com>
2017-02-12 20:17:57 -0700

    change export to export default

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import { namespace } from '~/src/dsl'
 import { destroy, get, head, patch, post, put, route } from '~/src/dsl/http'
 import { resources } from '~/src/dsl/resources'
 
-export const flauta = { register, resolve, namespace, destroy, get, head, patch, post, put, route, resources }
+export default { register, resolve, namespace, destroy, get, head, patch, post, put, route, resources }
